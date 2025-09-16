4f61913772dddb1d817388bf49d92eb7bd0cd360
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2025-09-16 12:56:07 +0200

    starting api

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image,Button, TouchableHighlight } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+} from "react-native";
 
-import icon from './assets/icon.png'
-
+import icon from "./assets/icon.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-        <TouchableHighlight
-          underlayColor={"#09f"}
-          onPress={() => (alert('Hola'))} 
-          style={{color : "red",width:150,height:100, borderRadius :10, backgroundColor:"green", justifyContent:'center', alignItems:'center'}}    
-        >
-           <Text style={{color :'#fff'}}>Bienvenid@ a liga A+7</Text>
-        </TouchableHighlight>
+      <TouchableHighlight
+        underlayColor={"#09f"}
+        onPress={() => alert("Hola")}
+        style={{
+          color: "red",
+          width: 150,
+          height: 100,
+          borderRadius: 10,
+          backgroundColor: "green",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "#fff" }}>Bienvenid@ a liga A+7</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -22,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
