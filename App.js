ba49aed4902d84fa9a5532c37c450a0128a19598
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2025-09-16 12:41:49 +0200

    Installing eslint

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,Button, TouchableHighlight } from 'react-native';
+
+import icon from './assets/icon.png'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bienvenid@ a liga A+7</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+        <TouchableHighlight
+          underlayColor={"#09f"}
+          onPress={() => (alert('Hola'))} 
+          style={{color : "red",width:150,height:100, borderRadius :10, backgroundColor:"green", justifyContent:'center', alignItems:'center'}}    
+        >
+           <Text style={{color :'#fff'}}>Bienvenid@ a liga A+7</Text>
+        </TouchableHighlight>
     </View>
   );
 }
@@ -13,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
